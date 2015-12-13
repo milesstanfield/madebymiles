9f9f95beaa488861fa8628d35ffb364f8e7ff452
Miles Stanfield <miles.stanfield@turner.com>
2015-12-13 01:56:20 -0500

    introduced first phase of home grown responsive grid system, splash area of home page styling

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper.rb"
 describe "home page", type: :feature do
   it "has a splash section" do
     visit "/"
-    within "#splash" do
+    within "[data-hook='splash-area']" do
       expect(page).to have_content("DEVELOPER") &&
         have_content("DESIGNER") &&
         have_content("TEACHER")
