a4886f6dcb00dd437f0616a48c411d993e202b8f
Miles Stanfield <miles.stanfield@turner.com>
2015-12-13 12:44:32 -0500

    added button to about section of homepage

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -22,7 +22,9 @@ describe "home page", type: :feature do
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
           printer took a galley of type and scrambled it to make a. It has survived not only five centuries,
           but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised
-          in the 1960s with the release of Letraset sheets containing")
+          in the 1960s with the release of Letraset sheets containing") &&
+        have_content("more") &&
+        have_css("a[href='/']")
     end
   end
 end
