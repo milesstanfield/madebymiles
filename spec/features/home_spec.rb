28192da8a0a30128db261999d6863ec42591e330
Miles Stanfield <miles.stanfield@turner.com>
2015-12-13 12:30:38 -0500

    about section styling homepage

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,12 +3,26 @@ require "spec_helper.rb"
 describe "home page", type: :feature do
   it "has a splash section" do
     visit "/"
-    within "[data-name='splash-area']" do
+    within "[data-name='splash']" do
       expect(page).to have_content("DEVELOPER") &&
         have_content("DESIGNER") &&
         have_content("TEACHER") &&
         have_content("Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text")
+    end
+  end
+
+  it "has an about section" do
+    visit "/"
+    within "[data-name='about']" do
+      expect(page).to have_content("about") &&
+        have_css("img[src='/assets/cog_orange.png']") &&
+        have_css("img[src='/assets/profile_head.png']") &&
+        have_content("Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+          printer took a galley of type and scrambled it to make a. It has survived not only five centuries,
+          but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised
+          in the 1960s with the release of Letraset sheets containing")
     end
   end
 end
