e03038ac1bb3107ff9f586fa07d0da54eaa7d452
Miles Stanfield <miles.stanfield@turner.com>
2015-12-13 02:02:09 -0500

    updated home page feature spec and moved navigation to app layout as partial

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,10 +4,12 @@ require "rails_helper.rb"
 describe "home page", type: :feature do
   it "has a splash section" do
     visit "/"
-    within "[data-hook='splash-area']" do
+    within "[data-name='splash-area']" do
       expect(page).to have_content("DEVELOPER") &&
         have_content("DESIGNER") &&
-        have_content("TEACHER")
+        have_content("TEACHER") &&
+        have_content("Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text")
     end
   end
 end
