95d8aad946d1b12a1e201949a84919e2990053a3
Miles Stanfield <miles.stanfield@turner.com>
2015-12-14 00:26:10 -0500

    quick update to db seeds for production (temporary)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 ["rails", "rspec", "git", "heroku", "bash", "haml", "scss"].each_with_index do |tag_name, index|
-  post = FactoryGirl.create(:post, title: "Why you should use #{tag_name}", use: "blog", created_at: Time.now - (index + 1).hours)
+  post = Post.create(title: "Why you should use #{tag_name}", use: "blog")
   post.tags.create(name: "#{tag_name}tag")
 end
