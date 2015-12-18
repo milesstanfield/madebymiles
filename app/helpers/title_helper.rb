a0e56f129b35ad5863af9e636bfec29f633c06d1
Miles Stanfield <miles.stanfield@turner.com>
2015-12-17 19:53:27 -0500

    removed slow cloud font usage

---
+++ app/helpers/title_helper.rb
@@ -0,0 +1,20 @@
+include ApplicationHelper
+
+module TitleHelper
+
+  def title_tag(c = controller)
+    content_tag(:title, "MadeByMiles#{append_portion(c)}")
+  end
+
+  private
+
+  def append_portion(controller)
+    case name_of_controller(controller)
+    when "home"
+    when "posts"
+      " | #{name_of_action(controller)} posts"
+    else
+      " | #{name_of_controller(controller)}"
+    end
+  end
+end
---
+++ spec/helpers/title_helper_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe TitleHelper do
+
+  context ".title_tag" do
+    it "returns title tag html for home page" do
+      view_context = double(:view_context, controller_name: "home", action_name: "index")
+      controller = double(:controller, view_context: view_context)
+      expect(title_tag(controller)).to eq "<title>MadeByMiles</title>"
+    end
+
+    it "returns title tag html for posts page" do
+      view_context = double(:view_context, controller_name: "posts", action_name: "tagged")
+      controller = double(:controller, view_context: view_context)
+      expect(title_tag(controller)).to eq "<title>MadeByMiles | tagged posts</title>"
+    end
+
+    it "returns title tag html for connect page" do
+      view_context = double(:view_context, controller_name: "connect", action_name: "index")
+      controller = double(:controller, view_context: view_context)
+      expect(title_tag(controller)).to eq "<title>MadeByMiles | connect</title>"
+    end
+  end
+end
