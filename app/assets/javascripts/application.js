07231eb2ffaf966746e9dd57c747de9eb134385b
Miles Stanfield <miles.stanfield@turner.com>
2015-12-31 23:57:44 -0500

    added mobile safe hovers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require hamburger
+//= require safe_hovers
---
+++ app/assets/javascripts/safe_hovers.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  if (!("ontouchstart" in document.documentElement)) {
+    document.documentElement.className += " no-touch";
+  }
+});
