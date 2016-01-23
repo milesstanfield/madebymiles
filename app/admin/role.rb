99dcbb9eb0ebd488ddb1c612cdb9011121791bce
Miles Stanfield <miles.stanfield@turner.com>
2016-01-23 12:18:40 -0500

    fixed bug where portfolio and roles wouldn't connect in db from active admin

--- app/admin/role.rb
+++ app/admin/role.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Role do
-  permit_params :title, :portfolio_id
+  permit_params :title, portfolio_ids: []
 
   index do
     selectable_column
@@ -10,7 +10,7 @@ ActiveAdmin.register Role do
   form do |f|
     f.inputs do
       f.input :title
-      f.input :portfolio, as: :check_boxes, collection: Portfolio.all
+      f.input :portfolios, as: :check_boxes, collection: Portfolio.all
     end
     f.actions
   end
