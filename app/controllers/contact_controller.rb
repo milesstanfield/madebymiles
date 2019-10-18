bc2d4bd22287c27c9e589d51ac098ec58e5ea3f4
miles <miles@callrail.com>
2019-10-18 15:01:58 -0400

    automated commit message

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -29,6 +29,20 @@ class ContactController < ApplicationController
     redirect_to contact_path message_params.merge(success: saved_message)
   end
 
+  def index_test2
+    page = Page.by_name_or_create "contact"
+    @active_nav_tab = page.active_nav_tab
+    @title_tag = page.title_tag
+    @meta_tags = page.meta_tags
+    @message = Message.new
+  end
+
+  def message_test2
+    saved_message = Message.new(message_params).save
+    saved_message ? flash_success_notice : flash_error_notice
+    redirect_to contact_path message_params.merge(success: saved_message)
+  end
+
   private
 
   def message_params
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   post "/contact", to: "contact#message", as: :messages
   get "/contact-test", to: "contact#index_test"
   post "/contact-test", to: "contact#message_test"
+  get "/contact-test2", to: "contact#index_test2"
+  post "/contact-test2", to: "contact#message_test2"
   get "/search", to: "search#index", as: :search
 
   get "/chat", to: "home#chat"
